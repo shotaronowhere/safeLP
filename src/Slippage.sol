ed24a0df44379f265724af0a16c8efafd4eb08ec
markus <33206087+markus0x1@users.noreply.github.com>
2024-07-13 21:37:36 +0200

    mintit

--- src/Slippage.sol
+++ src/Slippage.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.24;
 
 import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {Hooks} from "../lib/v4-core/src/libraries/Hooks.sol";
+import {SafeCast} from "../lib/v4-core/src/libraries/SafeCast.sol";
 import {IPoolManager} from "../lib/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "../lib/v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "../lib/v4-core/src/types/PoolId.sol";
 import {toBalanceDelta, BalanceDelta, BalanceDeltaLibrary} from "../lib/v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "../lib/v4-core/src/types/BeforeSwapDelta.sol";
 import {BaseClass} from "./BaseClass.sol";
+
+import {Currency, CurrencyLibrary} from "../lib/v4-core/src/types/Currency.sol";
 
 int256 constant PRECISION = 1e18;
 int24 constant MIN_TICK = -887220;
@@ -21,6 +24,9 @@ struct DeltaTokens {
 
 contract Slippage is BaseClass {
     using BalanceDeltaLibrary for BalanceDelta;
+    using SafeCast for *;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -60,7 +66,7 @@ contract Slippage is BaseClass {
 
     // trading operations
 
-    function _calcLiquidityCoef() internal view returns (int256) {
+    function _calcLiquidityCoef() internal pure returns (int256) {
         return 1e18;
     }
 
@@ -75,6 +81,15 @@ contract Slippage is BaseClass {
         int256 specifiedDelta = params.amountSpecified * liquidityCoef;
         // uint256 BeforeSwapDelta = BeforeSwapDeltaLibrary.toBeforeSwapDelta();
         // params.amountSpecified = params.amountSpecified * liquidityCoef;
+
+        require(specifiedDelta < 0, "must be input swap");
+
+        poolManager.mint(
+            usr,
+            key.currency0.toId() /* is this correct ?*/,
+            uint256(-specifiedDelta)
+        );
+
         return (
             BaseHook.beforeSwap.selector,
             BeforeSwapDeltaLibrary.ZERO_DELTA,
@@ -91,14 +106,23 @@ contract Slippage is BaseClass {
     ) internal virtual override returns (bytes4, int128) {
         super._afterSwap(usr, key, params, delta, data);
 
-        (int256 amount0Deflated, int256 amount1Deflated) = _deflateAmounts(
-            delta.amount0(),
-            delta.amount1()
-        );
-        int256 amount0Delta = amount0 - amount0Deflated;
-        int256 amount1Delta = amount1 - amount1Deflated;
+        int256 amount0Delta;
+        int256 amount1Delta;
 
-        poolManager.settle(outbound);
+        {
+            (int256 amount0, int256 amount1) = (
+                delta.amount0(),
+                delta.amount1()
+            );
+
+            (int256 amount0Deflated, int256 amount1Deflated) = _deflateAmounts(
+                amount0,
+                amount1
+            );
+            int256 amount0Delta = amount0 - amount0Deflated;
+            int256 amount1Delta = amount1 - amount1Deflated;
+        }
+        poolManager.burn(usr, key.currency0.toId(), uint256(-amount0Delta));
 
         return (BaseHook.afterSwap.selector, int128(amount1Delta));
     }
